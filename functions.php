54b796ac040ddfa5a3da5d68d93f428ebd8949a6
Trevorton27 <spiral272@gmail.com>
2019-09-28 20:50:56 +0900

    last commit from office Satruday night

--- functions.php
+++ functions.php
@@ -9,9 +9,3 @@ function university_files() {
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features() {
-    add_theme_support( 'title-tag' );
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-}
-
-add_action('after_setup_theme', 'university_features');
--- header.php
+++ header.php
@@ -14,17 +14,13 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-          <?php wp_nav_menu( array(
-            'theme_location' => 'headerMenuLocation'
-          )); 
-          ?>
-          <!--<ul>
-            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-            <li><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
-          </ul>-->
+          <ul>
+            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 18)  echo 'class="current-menu-item" ' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li <?php if (is_page('programs') or wp_get_post_parent_id(0) == 18)  echo 'class="current-menu-item" ' ?>><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
+            <li <?php if (is_page('events') or wp_get_post_parent_id(0) == 41)  echo 'class="current-menu-item" ' ?>><a href="<?php echo site_url('/events') ?>">Events</a></li>
+            <li <?php if (is_page('campuses') or wp_get_post_parent_id(0) == 43)  echo 'class="current-menu-item" ' ?>><a href="<?php echo site_url('/campuses') ?>">Campuses</a></li>
+            <li><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+          </ul>
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
